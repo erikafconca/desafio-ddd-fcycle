ecfaa6760755caf39c45145f70c3e69a858cb826
Erika Morais <erikafconca@gmail.com>
2024-10-17 15:36:44 -0300

    add order repository interface

--- src/domain/checkout/repository/order-repository.interface.ts
+++ src/domain/checkout/repository/order-repository.interface.ts
@@ -2,8 +2,8 @@ import RepositoryInterface from "../../@shared/repository/repository-interface";
 import Order from "../entity/order";
 
 export default interface OrderRepositoryInterface extends RepositoryInterface<Order> {
-    create(entity: T): Promise<void>;
-    update(entity: T): Promise<void>;
-    find(id: string): Promise<T>;
-    findAll(): Promise<T[]>;
+    create(entity: Order): Promise<void>;
+    update(entity: Order): Promise<void>;
+    find(id: string): Promise<Order>;
+    findAll(): Promise<Order[]>;
 }
